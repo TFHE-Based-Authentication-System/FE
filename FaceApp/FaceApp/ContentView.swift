21e0433bd5e9fab624959a84b9e739bf5ef072f6
minykang <algh5304@naver.com>
2025-03-26 16:08:08 +0900

    docs: Add initial project overview to README

---
+++ FaceApp/FaceApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FaceApp
+//
+//  Created by 강민영 on 3/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FaceApp/FaceApp/FaceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceAppApp.swift
+//  FaceApp
+//
+//  Created by 강민영 on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct FaceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
